5b11f1aabf59ec40b7c2e8dbffe23f6363dc60b2
Merchel <RRece@web.de>
2016-09-30 00:49:40 +0200

    Funktionen hinzugefügt

--- bot.c
+++ bot.c
@@ -77,6 +77,106 @@ void log_file(const char name[],const char channel[],const char text[])
 
 }
 
+//Konfigurationsdatei laden
+int configfileLaden(char * filen)
+{
+	printf(" : - Lade aus %s \n",filen);
+
+	configFile = fopen(filen,"r");
+
+	if(configFile == NULL)
+	{
+		printf("X: - Bot konnte Configfile nicht lesen... \n");
+		fclose(configFile);
+		return 0;
+	}
+	 
+	char tmp[BUFFER_SIZE];
+
+	while(fgets(configLine, BUFFER_SIZE, configFile) != NULL)
+	{
+		sprintf(tmp,"%s",configLine);
+	}
+
+	fclose(configFile);
+
+	char params[10][100];
+	int i=0;
+
+	char *ptr =strtok(tmp,";");
+
+	while(ptr !=NULL)
+	{
+		sprintf(params[i],"%s",ptr);
+		ptr = strtok(NULL, ";");
+		i++;
+	}
+
+	server = params[0];
+	ctx.nick = params[1];
+	ctx.channel = params[2];
+
+	return 1;
+}
+
+//setzt Channel Einstellungen
+void setChannelSettings(const char channel[],const unsigned int settings)
+{
+	int i;
+	for(i=0;i<MAX_CHANNELS;i++)
+	{
+		if(strstr(chansettings[i].channel,channel))
+		{
+			chansettings[i].settings = settings;
+			break;
+		}
+	}
+}
+
+//verläst Channel
+void rmChannel(const char channel[])
+{
+	int i;
+	for(i=0;i<MAX_CHANNELS;i++)
+	{
+	if(strstr(chansettings[i].channel,channel))
+		{
+			chansettings[i].channel[0] = 'X';
+			break;
+		}
+	}
+}
+
+//Channel hinzufügen
+void addChannelSettings(const char channel[],const unsigned int settings)
+{
+	strcpy(chansettings[(currentChannelCount-1)].channel,channel);
+	chansettings[(currentChannelCount-1)].settings = settings;
+	printf(" : - %s config set = %d\n",chansettings[(currentChannelCount-1)].channel,chansettings[(currentChannelCount-1)].settings);
+}
+
+//Git Channel Einstellungen aus
+unsigned int getChannelSettings(const char channel[])
+{
+	int i;
+	for(i=0;i<MAX_CHANNELS;i++)
+	{
+		//printf("%c-\n",chansettings[i].channel[0] );
+		if(chansettings[i].channel[0] != '#')
+		{
+			addChannelSettings(channel,DEFAULT_CHANNEL_SETTINGS);
+			return DEFAULT_CHANNEL_SETTINGS;
+		}
+		// printf("%s=%s",channel,chansettings[i].channel);
+
+		if(strcmp(channel,chansettings[i].channel) == 0)
+		{
+			return chansettings[i].settings;
+		}	
+	}
+	return(0);
+}
+
 
 int main()
 {
