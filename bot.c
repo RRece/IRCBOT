59f7e877dd851beda84c81f608577d484bff5a99
Merchel <RRece@web.de>
2016-09-30 02:49:41 +0200

    Hauptfunktion hinzugefügt

--- bot.c
+++ bot.c
@@ -336,10 +336,150 @@ void sql_geturl(irc_session_t *session,const char name[],int counter)
 		break;
         }
         sqlite3_finalize(vm);
-
 }
 
-int main()
+//Connectfunktion 
+void event_connect (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
 {
+	sprintf(botnick,"%s",ctx.nick);
+	irc_ctx_t * ctx = (irc_ctx_t *) irc_get_ctx (session);
+	irc_cmd_join (session, ctx->channel, 0);
+	currentChannelCount++;
+}
+
+//Channel beitritt
+void event_join (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
+{
+	irc_cmd_user_mode (session, "+i");
+	//irc_cmd_msg (session, params[0], "Hallo");
+}
+
+//Privat Nachricht
+void event_privmsg (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
+{
+	//printf ("'%s' said me (%s): %s\n", origin ? origin : "someone", params[0], params[1] );
+	if(privmsg_settings & LOG_TXT)
+	log_file(origin,"privmsg",params[1]);
+
+	if(privmsg_settings & URL_SQL && (strstr(params[1],"http://") || strstr(params[1],"www.") ))
+	{
+		sql_addurl(origin,"privmsg",params[1]);
+	}	
+
+	ircCommands(session,origin,params,privmsg_settings);
+}
+
+//im Channel
+void event_channel (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
+{
+	//printf("'%s' said in channel %s: %s\n",origin ? origin : "someone",params[0],params[1]);
+	int tmpsettings = getChannelSettings(params[0]);
+
+	if(tmpsettings & LOG_TXT)
+	log_file(origin,params[0],params[1]);
+
+	if(tmpsettings & URL_SQL && (strstr(params[1],"http://") || strstr(params[1],"www.") ))
+	{
+		sql_addurl(origin,params[0],params[1]);
+	}
+
+	if(tmpsettings & URL_SQL && strstr(params[1],"!geturls"))
+	sql_geturl(session,origin,5);
+
+	ircCommands(session,origin,params,tmpsettings);
+}
+
+
+//Hauptfunktion
+int main(int argc, char** argv)
+{
+	irc_callbacks_t callbacks;
+	irc_session_t *s;
+
+	//Zeit
+	time ( &rawtime );
+	ptm = gmtime ( &rawtime );
+
+	printf(" : - Bot wird gestartet...\n");
+
+	//SQLite
+	printf(" : - SQLite wird initialisiert...\n");
+	sqlite3_open(SQLITEFILE, &sqlitedb);
+	sql_createtables();
+
+	switch(argc)
+	{
+	case 2:
+		{
+		int tmp = -1;
+		tmp = configfileLaden(argv[1]);
+
+		if(tmp==0)
+		{
+		return 1;
+		}
+			printf(" : - Bot Configfile geladen \n");
+		}
+		break;
+	case 4:
+		ctx.channel = argv[3];
+		ctx.nick = argv[2];
+		server	= argv[1];
+		printf(" : - Bot configuriert (Parameter) \n");
+		break;
+	default:
+	printf ("!: - Parameter fehlen\n");
+	printf ("!: - %s <server> <nick> '<#channel>' \n",argv[0]);
+	printf ("!: - %s <configfile> \n",argv[0]);
+	return 1;
+	}
+
+	printf(" : - Server: %s \n",server);
+	printf(" : - Nick: %s \n",ctx.nick);
+	printf(" : - Channel: %s \n",ctx.channel);
+
+	memset(&callbacks, 0, sizeof(callbacks));
+
+	//Events
+	callbacks.event_connect = event_connect;
+	callbacks.event_join = event_join;
+	callbacks.event_channel = event_channel;
+	callbacks.event_privmsg = event_privmsg;
+	
+	// ctx.channel = "#Bot";
+	// ctx.nick = "FH-BoT";
+
+	if(server == NULL)
+	{
+		printf("?: - setzt Standartserver\n");
+		server = "localhost";
+	}
+
+	s = irc_create_session(&callbacks);
+
+	if(!s)
+	{
+		printf("X: - Konnte keine IRC Session aufbauen...\n");
+		return 1;
+	}
+
+	irc_set_ctx (s,&ctx);
+	irc_option_set (s, LIBIRC_OPTION_STRIPNICKS);
+
+	if( irc_connect (s, server,port, 0,ctx.nick,0,0))
+	{
+		printf("X: - Konnte keine Verbindung zum Server aufbauen...\n");
+		return 1;
+	}	
+
+	if( irc_run (s) )
+	{
+		printf (" : - SQLite wird geschlossen. \n");
+		sqlite3_close(sqlitedb);
+		printf (" : - Bot wurde beendet. \n");
+		return 1;
+	}
+
+	sqlite3_close(sqlitedb);
 	return 1;
 }
